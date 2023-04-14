d03aeb92da964a3384f24f64728d2214e3ede811
Gabriel1519 <130614709+Gabriel1519@users.noreply.github.com>
2023-04-14 20:06:44 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1Ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double M;
+            double km;
+
+            Console.Write("Insira a Distancia em milhias Maritimas: ");
+            M = double.Parse(Console.ReadLine());
+
+           km = M * 0.5399;
+
+            Console.WriteLine("A Distancia em KM é Aproximadamente {0}.", km);
+        }
+    }
+}
